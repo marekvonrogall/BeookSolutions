b67033d09ef24edda457ae98ce51835d89d7c5cd
Marek Mika von Rogall <marekvonrogall@gmx.ch>
2024-08-26 22:24:22 +0200

    Changed file structure. Removed JS.

--- BeookSolutions_Website/script.js
+++
@@ -1,5 +0,0 @@
-function downloadFile() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    document.querySelector('.big-text').textContent = "Vielen Dank für's herunterladen!";
-    window.location.href = 'https://github.com/marekvonrogall/BeookSolutions/releases/download/1.0.0.2/BeookSolutions.zip';
-}
